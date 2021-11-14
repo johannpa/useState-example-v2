8bd17046ff5006cbf0b7c41ea6db9c07f95b088c
johannpa <johann_paque@hotmail.fr>
2021-11-14 17:08:23 +0100

    useState with multiple cases

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [age, setAge] = useState(42);
+  const [fruit, setFruit] = useState('banana');
+  const [todos, setTodos] = useState([{ text: 'Learn Hooks' }]);
+
+  const handleAgeClick = () => {
+    setAge(age + 1);
+  }
+
+  const handleOrangeClick = () => {
+    setFruit('orange');
+  }
+  const handleTodosClick = () => {
+    setTodos([{text: 'Learn Cooking'}]);
+  }
+
+  
+  
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>This your age: {age}</p>
+        <button onClick={handleAgeClick}>Add 1 year</button>
+        <br />
+        <p>You like this fruit: {fruit}</p>
+        <button onClick={handleOrangeClick}>Change fruit</button>
+        <br />
+        <p>My work for today</p>
+        {todos.map((todo, i) => <div key={i}>{todo.text}</div>)}
+        <br />
+        <button onClick={handleTodosClick}>Next task</button>
       </header>
     </div>
   );
